01da47e9ea31c7d441c483cd89c745b4c5175b24
Steven Ward <stevenward94@gmail.com>
2017-05-27 17:11:49 -0500

    Update 'core.clj' source file for 'chapter-7'
    Continue work on the 'chapter-7' project for Chapter 7 of CFTBAT. This
    commit updates 'core.clj' under the chapter's 'src/' directory.
    
    NOTE: 'chapter-7/src/chapter_7/core.clj' continues to be INCOMPLETE as
    of this commit...

--- chapter-7/src/chapter_7/core.clj
+++ chapter-7/src/chapter_7/core.clj
@@ -7,3 +7,30 @@
 
 
 (def addition-list (list + 1 2))
+
+(eval (list 'def 'lucky-number (concat addition-list [10])))
+
+
+(def three (read-string "(+ 1 2)"))
+
+
+;; These things evaluate to themselves:
+;    true
+;; => true
+;;
+;    false
+;; => false
+;;
+;    {}
+;; => {}
+;;
+;    :huzzah
+;; => :huzzah
+;;
+;    ()
+;; => ()
+
+
+(defn exclaim
+  [exclamation]
+  (str exclamation "!"))
